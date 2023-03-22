31422bc5919f6aa7281ceb23890d4c127fde1888
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2023-03-22 16:23:30 +0530

    Added comment

--- Strings/inputVowelsOnly.c
+++ Strings/inputVowelsOnly.c
@@ -1,3 +1,14 @@
+/*
+    SAMPLE INPUT:
+        Enter vowels only (first non-vowel character will stop the input) : ABHINAV
+    SAMPLE OUTPUT:
+        A
+
+    SAMPLE INPUT:
+        Enter vowels only (first non-vowel character will stop the input) : aeiouAEIOU
+    SAMPLE OUTPUT:
+        aeiouAEIOU
+*/
 #include<stdio.h>
 
 int main(){
