ef9f11c5ecff36a29a2b56462f3fe10286ab001e
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2023-03-26 18:19:33 +0530

    No change

--- Structures/1.c
+++ Structures/1.c
@@ -16,12 +16,9 @@ int main(){
     p1.name = "Abhinav";
     p1.age = 19;
     p1.salary = 25000.0;
-
     printf("Name_1 : %s, Age_1 : %d, Salary_1 : %.2f\n", p1.name, p1.age, p1.salary);
 
-
     struct person p2 = {"Someone_you_love", 19, 1000000};
-
     printf("Name_2 : %s, Age_2 : %d, Salary_2 : %.2f", p2.name, p2.age, p2.salary);
 
     return 0;
